44bcab088772f22104158a9b926134951bf97d45
Abraham MVE <mveabraham@gmail.com>
2022-08-02 15:04:29 +0100

    first commit

---
+++ config/repository-generator.php
@@ -0,0 +1,58 @@
+<?php
+
+return [
+
+    'user_model_class' => 'App\\Models\\User',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Directories
+    |--------------------------------------------------------------------------
+    |
+    | The default directory structure
+    |
+    */
+
+    'models_directory' => app_path('Models/'),
+    'contracts_directory' => app_path('Contracts/'),
+    'repositories_directory' => app_path('Repositories/'),
+    'policies_directory' => app_path('Policies/'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Namespaces
+    |--------------------------------------------------------------------------
+    |
+    | The namespace of repository and models
+    |
+    */
+    'models_namespace' => 'App\Models',
+    'contracts_namespace' => 'App\Contracts',
+    'repositories_namespace' => 'App\Repositories',
+    'policies_namespace' => 'App\Policies',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Main Repository File
+    |--------------------------------------------------------------------------
+    |
+    | The main repository class, other repositories will be extended from this
+    |
+    | If you're working with your customized repository file
+    | You should change these values like below,
+    |
+    | 'base_repository_file' => 'CustomFile.php'
+    | 'base_repository_class' => 'App\Custom\Repository:class'
+    */
+
+    // Only file name of the file because full path can cause errors.
+    // We're going to use "repository_directory" config value for it.
+    'base_repository_file' => 'BaseRepository.php',
+    // Class name as string
+    'base_repository_class' => \Lab2view\RepositoryGenerator\BaseRepository::class,
+
+    // We're going to use "contracts_directory" config value for it.
+    'base_contract_file' => 'RepositoryInterface.php',
+    // Interface name as string
+    'base_contract_interface' => \Lab2view\RepositoryGenerator\RepositoryInterface::class,
+];
---
+++ src/RepositoryInterface.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace Lab2view\RepositoryGenerator;
+
+interface RepositoryInterface
+{
+    /**
+     * @param string $key
+     * @param $value
+     * @param bool $withTrashed
+     * @return mixed
+     */
+    public function exists(string $key, $value, bool $withTrashed = false) : bool;
+
+    /**
+     * @param string $attr_name
+     * @param mixed $attr_value
+     * @param array $relations
+     * @param bool $withTrashed
+     * @param array $selects
+     * @return mixed
+     */
+    public function getByAttribute(
+        string $attr_name,
+        $attr_value,
+        array $relations = [],
+        bool $withTrashed = false,
+        array $selects = []
+    );
+
+    /**
+     * @param int $n
+     * @param array $relations
+     * @param bool $withTrashed
+     * @param array $selects
+     * @return mixed
+     */
+    public function getPaginate(int $n, array $relations = [], bool $withTrashed = false, array $selects = []);
+
+    /**
+     * @param array $inputs
+     * @return mixed
+     */
+    public function store(Array $inputs);
+
+    /**
+     * @param $id
+     * @param array $relations
+     * @param bool $withTrashed
+     * @param array $selects
+     * @return mixed
+     */
+    public function getById($id, array $relations = [], bool $withTrashed = false, array $selects = []);
+
+    /**
+     * @param $key
+     * @param $value
+     * @param array $relations
+     * @param bool $withTrashed
+     * @param array $selects
+     * @return mixed
+     */
+    public function search($key, $value, array $relations = [], bool $withTrashed = false, array $selects = []);
+
+    /**
+     * @param array $relations
+     * @param bool $withTrashed
+     * @param array $selects
+     * @return mixed
+     */
+    public function getAll(array $relations = [], bool $withTrashed = false, array $selects = []);
+
+    /**
+     * @param bool $withTrashed
+     * @return mixed
+     */
+    public function countAll(bool $withTrashed = false);
+
+    /**
+     * @param $key
+     * @return mixed
+     */
+    public function getAllSelectable($key);
+
+    /**
+     * @param $id
+     * @param array $inputs
+     * @return mixed
+     */
+    public function update($id, Array $inputs);
+
+    /**
+     * @param $id
+     * @return bool
+     */
+    public function destroy($id): bool;
+
+    /**
+     * @return bool
+     */
+    public function destroyAll(): bool;
+
+    /**
+     * @param $id
+     * @return bool
+     */
+    public function forceDelete($id): bool;
+
+    /**
+     * @param $id
+     * @return bool
+     */
+    public function restore($id): bool;
+}
